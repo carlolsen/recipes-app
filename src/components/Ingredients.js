3e72c8bb0c126af7bf6e8ba8862eb43056e71413
Carl Olsen <carl-olsen@mchsi.com>
2020-07-18 13:03:56 -0500

    first version of files

---
+++ src/components/Ingredients.js
@@ -0,0 +1,11 @@
+// ./src/components/Ingredients.js
+
+import React from "react";
+
+export default function Ingredients({ amount, measurement, name }) {
+    return (
+        <li>
+            {amount} {measurement} {name}
+        </li>
+    )
+}
